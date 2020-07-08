635b1f25396080f7486bec26994defd4b2a149de
Franco Meloni <franco.meloni91@gmail.com>
2020-07-08 23:51:53 +0100

    Update Dangerfile.df.kts

--- Dangerfile.df.kts
+++ Dangerfile.df.kts
@@ -1,22 +1,6 @@
-#!/usr/bin/env kscript
+import systems.danger.kotlin.*
 
-//@file:DependsOn("com.danger:danger-kotlin-library:0.1.0")
+danger(args) {
 
-import com.danger.dangerkotlin.*
-
-val danger = Danger(args)
-
-val allSourceFiles = danger.git.modifiedFiles + danger.git.createdFiles
-
-val sourceChanges = allSourceFiles.firstOrNull { it.contains("src")  }
-val isTrivial = danger.github!!.pullRequest.title.contains("#trivial")
-
-if (danger.git.createdFiles.size + danger.git.modifiedFiles.size - danger.git.deletedFiles.size > 10) {
-    warn("Big PR, try to keep changes smaller if you can")
+    warn("Test")
 }
-
-if (danger.github!!.pullRequest.title.contains("WIP" ,false)) {
-    warn("PR is classed as Work in Progress")
-}
-
-warn("TEST2")
